2bb163c5b85f9b0b180fa731bdf55ff74289a685
omaidy <aguilaromaly@gmail.com>
2025-02-21 17:36:39 -0500

    INICIO

---
+++ src/main/java/api/ieie037santarosa/edu/pe/intranet_api/IntranetApiApplication.java
@@ -0,0 +1,13 @@
+package api.ieie037santarosa.edu.pe.intranet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntranetApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/ieie037santarosa/edu/pe/intranet_api/IntranetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.ieie037santarosa.edu.pe.intranet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
